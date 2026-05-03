2099de008ab867dd242d0aac013d14c3065c3c5a
Sander Teunissen <sander@fam-teunissen.nl>
2026-05-03 09:52:29 +0200

    apparently no meta package present, add all xfce4 packages for now, can select necessary ones later

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
-dnf5 install -y xfce4-desktop
+dnf5 install -y xfce4-*
 
 # Use a COPR Example:
 # dnf5 -y copr enable ublue-os/staging
