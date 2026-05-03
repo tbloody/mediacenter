2f20895dfd457e00c35795264edea73288519c12
Sander Teunissen <sander@fam-teunissen.nl>
2026-05-03 09:09:16 +0200

    added xfce and kodi

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,13 +11,14 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
+dnf5 install -y xdce4-desktop
+dnf5 install -y xdce4-desktop
 
 # Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+ dnf5 -y copr enable ublue-os/staging
+ dnf5 -y install kodi
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+ dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
