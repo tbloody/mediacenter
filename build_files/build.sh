44f809b548113aced045477d218dd6e7d131f88e
sander <sander@localhost.localdomain>
2026-05-18 09:58:33 +0200

    alright, let's try installing kodi again

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,6 +20,10 @@ dnf5 install -y nvim
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -yy install kodi
+dnf5 -y remove rpmfusion-free-release
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
