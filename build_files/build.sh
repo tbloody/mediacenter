59073e7bb23b1d3352113392c23b9c7b9314f747
sander <sander@localhost.localdomain>
2026-05-18 22:36:53 +0200

    assuming error is off and is because the other disable call

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,7 @@ dnf5 install -y nvim
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
- dnf5 -y copr disable ublue-os/staging
+# dnf5 -y copr disable ublue-os/staging
 
 sudo dnf5 copr enable @kdesig/kde-beta
 
